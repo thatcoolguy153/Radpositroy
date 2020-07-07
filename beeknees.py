ef485025c544dbe023e5684561851f70de9b1588
thatcoolguy153 <67939577+thatcoolguy153@users.noreply.github.com>
2020-07-07 00:20:42 -0400

    Add files via upload

---
+++ beeknees.py
@@ -0,0 +1,11 @@
+
+bees = raw_input("Hello, if you could describe how good you day is going in number of bees, how many bees would that bee?")
+bees = int(bees)
+if bees < 1:
+  print "dude that awful. You day knees work"
+elif bees < 10:
+  bk = 6 * bees
+  print "Well by my carefull calculations, thats %d beesknees my friend! Enjoy!" % bk
+else:
+  print "Geeeezzzeeee, that be more knees from a bee then I can see! whoopee!!!"
+  
